bfe39875f5201300fb77fe3d748ec79063deda05
olawanlejoel <info4tobest2@gmail.com>
2023-11-09 10:13:24 +0100

    add new

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
